a859b1f5f97fa3b8fb05c98ec296c9e046b08b37
Marvin <52848568+mleem97@users.noreply.github.com>
2026-07-07 22:38:01 +0200

    music: document configuration type

--- plugins/MusicDashboard/Configuration/PluginConfiguration.cs
+++ plugins/MusicDashboard/Configuration/PluginConfiguration.cs
@@ -2,7 +2,13 @@ using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.MusicDashboard.Configuration;
 
+/// <summary>
+/// Configuration values for Music Dashboard.
+/// </summary>
 public class PluginConfiguration : BasePluginConfiguration
 {
+    /// <summary>
+    /// Gets or sets inclusion of non-music libraries in the overview.
+    /// </summary>
     public bool IncludeNonMusicLibraries { get; set; }
 }
