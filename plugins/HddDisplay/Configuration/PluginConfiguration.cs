339527101c426becdca26999facd74f62f8c9cbd
Marvin <52848568+mleem97@users.noreply.github.com>
2026-07-07 21:55:48 +0200

    refactor: move HddDisplay configuration

---
+++ plugins/HddDisplay/Configuration/PluginConfiguration.cs
@@ -0,0 +1,28 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.HddDisplay.Configuration;
+
+/// <summary>
+/// Configuration for HDD Display.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        IncludeSystemDrive = false;
+        DashboardRefreshSeconds = 5;
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the system/root drive should be shown.
+    /// </summary>
+    public bool IncludeSystemDrive { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Admin Dashboard widget refresh interval in seconds.
+    /// </summary>
+    public int DashboardRefreshSeconds { get; set; }
+}
